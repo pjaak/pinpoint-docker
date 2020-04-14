72c8c50cd76af045146fe830e060246a38491910
Aaron Pejakovic <aaron.pejakovic@outlook.com>
2020-04-14 14:05:52 +1000

    Added some collector things

--- pinpoint-collector/build/scripts/start-collector.sh
+++ pinpoint-collector/build/scripts/start-collector.sh
@@ -10,6 +10,12 @@ sed -i "/flink.cluster.zookeeper.address=/ s/=.*/=${FLINK_CLUSTER_ZOOKEEPER_ADDR
 sed -i "/hbase.client.host=/ s/=.*/=${HBASE_HOST}/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/profiles/${SPRING_PROFILES}/hbase-env.properties
 sed -i "/hbase.client.port=/ s/=.*/=${HBASE_PORT}/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/profiles/${SPRING_PROFILES}/hbase-env.properties
 
+# Added this in to use TCP instead of UDP for collector
+sed -i "/collector.receiver.stat.tcp=/ s/=.*/=true/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
+sed -i "/collector.receiver.stat.udp=/ s/=.*/=false/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
+sed -i "/collector.receiver.span.udp=/ s/=.*/=false/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
+sed -i "/collector.receiver.span.tcp=/ s/=.*/=true/" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
+
 sed -i "/level value=/ s/=.*/=\"${DEBUG_LEVEL}\"\/>/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/profiles/${SPRING_PROFILES}/log4j.xml
 
 exec /usr/local/tomcat/bin/catalina.sh run
